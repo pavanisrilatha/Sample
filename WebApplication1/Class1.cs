48fb7367ba6663a84d26db556c1d5453c71564e1
Pavani Srilatha Kolla <pskolla@tollplus.com>
2019-12-11 12:49:20 +0530

    added class1

---
+++ WebApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Class1
+    {
+        public void getData()
+        {
+            Console.WriteLine("github");
+            Console.Read();
+        }
+    }
+}
