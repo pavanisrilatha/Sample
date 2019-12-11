026dddfe1ce05a64314472c1d82f4c8fc67888cf
Pavani Srilatha Kolla <pskolla@tollplus.com>
2019-12-11 12:13:26 +0530

    new sample  class added

---
+++ WebApplication1/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Class
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("github");
+            Console.Read();
+        }
+    }
+}
